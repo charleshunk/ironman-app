893b19147bf9b3db3c07d60b9b136c3c4129341f
Carlos Galan <carlos.galan@gmail.com>
2020-03-24 20:26:52 +0000

    Updates

---
+++ precache-manifest.08470ed872e4718ec91c8356f3b53f7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b9df84ef762ba91a8a050a67e4deb1",
+    "url": "/ironman-app/index.html"
+  },
+  {
+    "revision": "608c86312883bd08c3ea",
+    "url": "/ironman-app/static/css/main.21e79708.chunk.css"
+  },
+  {
+    "revision": "4b211e2b1cfc2bd96a53",
+    "url": "/ironman-app/static/js/2.1b20f9af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ironman-app/static/js/2.1b20f9af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608c86312883bd08c3ea",
+    "url": "/ironman-app/static/js/main.8ccccd66.chunk.js"
+  },
+  {
+    "revision": "82721e8284131cd2fba1",
+    "url": "/ironman-app/static/js/runtime-main.b975c3b0.js"
+  }
+]);
